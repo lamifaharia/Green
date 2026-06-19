fad8dd320624afb6b241ea38462285d53e43f71d
lamifaharia <lamifaharia@gmail.com>
2026-06-19 21:11:43 +0600

    add cart quantity management and totals

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -11,16 +11,81 @@ const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
 
   const addToCart = (product) => {
-    setCart((prev) => [...prev, product]);
+    const existingProduct = cart.find(
+      (item) => item.id === product.id
+    );
+
+    if (existingProduct) {
+      setCart(
+        cart.map((item) =>
+          item.id === product.id
+            ? {
+                ...item,
+                quantity: item.quantity + 1,
+              }
+            : item
+        )
+      );
+    } else {
+      setCart([
+        ...cart,
+        {
+          ...product,
+          quantity: 1,
+        },
+      ]);
+    }
 
     toast.success(`${product.name} added to cart`);
   };
 
+  const increaseQuantity = (id) => {
+    setCart(
+      cart.map((item) =>
+        item.id === id
+          ? {
+              ...item,
+              quantity: item.quantity + 1,
+            }
+          : item
+      )
+    );
+  };
+
+  const decreaseQuantity = (id) => {
+    setCart(
+      cart
+        .map((item) =>
+          item.id === id
+            ? {
+                ...item,
+                quantity: item.quantity - 1,
+              }
+            : item
+        )
+        .filter((item) => item.quantity > 0)
+    );
+  };
+
   const removeFromCart = (id) => {
-    setCart(cart.filter((item) => item.id !== id));
+    setCart(
+      cart.filter((item) => item.id !== id)
+    );
 
     toast.error("Item removed");
   };
+
+  const totalPrice = cart.reduce(
+    (total, item) =>
+      total + item.price * item.quantity,
+    0
+  );
+
+  const totalItems = cart.reduce(
+    (total, item) =>
+      total + item.quantity,
+    0
+  );
 
   return (
     <CartContext.Provider
@@ -28,6 +93,10 @@ const CartProvider = ({ children }) => {
         cart,
         addToCart,
         removeFromCart,
+        increaseQuantity,
+        decreaseQuantity,
+        totalPrice,
+        totalItems,
       }}
     >
       {children}
