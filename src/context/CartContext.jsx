a86d727e43cf2072370401f1b093000ccf1391a9
lamifaharia <lamifaharia@gmail.com>
2026-06-23 22:29:39 +0600

    update cartcontext

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,119 +1,150 @@
-import { createContext, useContext, useState } from "react";
+import {
+createContext,
+useContext,
+useState,
+useEffect,
+} from "react";
 import toast from "react-hot-toast";
 
 const CartContext = createContext();
 
 export const useCart = () => {
-  return useContext(CartContext);
+return useContext(CartContext);
 };
 
 const CartProvider = ({ children }) => {
-  const [cart, setCart] = useState([]);
+const [cart, setCart] = useState(() => {
+const savedCart =
+localStorage.getItem("cart");
 
-  // Add Product
-  const addToCart = (product) => {
-    setCart((prev) => {
-      const existingProduct = prev.find(
-        (item) => item.id === product.id
-      );
+```
+return savedCart
+  ? JSON.parse(savedCart)
+  : [];
+```
 
-      if (existingProduct) {
-        return prev.map((item) =>
-          item.id === product.id
-            ? {
-                ...item,
-                quantity: item.quantity + 1,
-              }
-            : item
-        );
-      }
+});
 
-      return [
-        ...prev,
-        {
-          ...product,
-          quantity: 1,
-        },
-      ];
-    });
+useEffect(() => {
+localStorage.setItem(
+"cart",
+JSON.stringify(cart)
+);
+}, [cart]);
 
-    toast.success(`${product.name} added to cart`);
-  };
+const addToCart = (product) => {
+const existingProduct = cart.find(
+(item) => item.id === product.id
+);
 
-  // Increase Quantity
-  const increaseQuantity = (id) => {
-    setCart((prev) =>
-      prev.map((item) =>
-        item.id === id
-          ? {
-              ...item,
-              quantity: item.quantity + 1,
-            }
-          : item
-      )
-    );
-  };
-
-  // Decrease Quantity
-  const decreaseQuantity = (id) => {
-    setCart((prev) =>
-      prev
-        .map((item) =>
-          item.id === id
-            ? {
-                ...item,
-                quantity: item.quantity - 1,
-              }
-            : item
-        )
-        .filter((item) => item.quantity > 0)
-    );
-  };
-
-  // Remove Product
-  const removeFromCart = (id) => {
-    setCart((prev) =>
-      prev.filter((item) => item.id !== id)
-    );
-
-    toast.error("Item removed");
-  };
-
-  // Clear Cart
-  const clearCart = () => {
-    setCart([]);
-    toast.success("Cart cleared");
-  };
-
-  // Total Items
-  const totalItems = cart.reduce(
-    (total, item) => total + item.quantity,
-    0
+```
+if (existingProduct) {
+  setCart(
+    cart.map((item) =>
+      item.id === product.id
+        ? {
+            ...item,
+            quantity:
+              item.quantity + 1,
+          }
+        : item
+    )
   );
+} else {
+  setCart([
+    ...cart,
+    {
+      ...product,
+      quantity: 1,
+    },
+  ]);
+}
 
-  // Total Price
-  const totalPrice = cart.reduce(
-    (total, item) =>
-      total + item.price * item.quantity,
-    0
-  );
+toast.success(
+  `${product.name} added to cart`
+);
+```
 
-  return (
-    <CartContext.Provider
-      value={{
-        cart,
-        addToCart,
-        removeFromCart,
-        increaseQuantity,
-        decreaseQuantity,
-        clearCart,
-        totalItems,
-        totalPrice,
-      }}
-    >
-      {children}
-    </CartContext.Provider>
-  );
+};
+
+const increaseQuantity = (id) => {
+setCart(
+cart.map((item) =>
+item.id === id
+? {
+...item,
+quantity:
+item.quantity + 1,
+}
+: item
+)
+);
+};
+
+const decreaseQuantity = (id) => {
+setCart(
+cart
+.map((item) =>
+item.id === id
+? {
+...item,
+quantity:
+item.quantity - 1,
+}
+: item
+)
+.filter(
+(item) => item.quantity > 0
+)
+);
+};
+
+const removeFromCart = (id) => {
+setCart(
+cart.filter(
+(item) => item.id !== id
+)
+);
+
+```
+toast.error("Item removed");
+```
+
+};
+
+const clearCart = () => {
+setCart([]);
+};
+
+const totalPrice = cart.reduce(
+(total, item) =>
+total +
+item.price * item.quantity,
+0
+);
+
+const totalItems = cart.reduce(
+(total, item) =>
+total + item.quantity,
+0
+);
+
+return (
+<CartContext.Provider
+value={{
+cart,
+addToCart,
+removeFromCart,
+increaseQuantity,
+decreaseQuantity,
+clearCart,
+totalPrice,
+totalItems,
+}}
+>
+{children}
+</CartContext.Provider>
+);
 };
 
 export default CartProvider;
