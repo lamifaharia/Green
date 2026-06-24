d55e62b1faad1d8bd0a7e9bc6340988cef58de64
lamifaharia <lamifaharia@gmail.com>
2026-06-24 22:09:07 +0600

    OrdersContext

---
+++ src/context/OrdersContext.jsx
@@ -0,0 +1,46 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+const OrdersContext = createContext();
+
+export const useOrders = () => {
+  return useContext(OrdersContext);
+};
+
+const OrdersProvider = ({ children }) => {
+  const [orders, setOrders] = useState(() => {
+    const savedOrders = localStorage.getItem("orders");
+
+    ```
+return savedOrders
+  ? JSON.parse(savedOrders)
+  : [];
+```;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("orders", JSON.stringify(orders));
+  }, [orders]);
+
+  const addOrder = (order) => {
+    setOrders((prev) => [
+      ...prev,
+      {
+        id: Date.now(),
+        ...order,
+      },
+    ]);
+  };
+
+  return (
+    <OrdersContext.Provider
+      value={{
+        orders,
+        addOrder,
+      }}
+    >
+      {children}
+    </OrdersContext.Provider>
+  );
+};
+
+export default OrdersProvider;
